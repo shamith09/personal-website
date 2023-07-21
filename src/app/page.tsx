c70ded7e0eb4633074a144e1dc04574953d030fc
Shamith Pasula <shamith09@gmail.com>
2023-07-21 15:00:59 -0700

    clear

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import SPLogo from "../../public/Logo.svg";
+import NavBar from "@/components/NavBar";
 
 export default function Home() {
   return (
     <>
-      <Image src={SPLogo} alt="logo" />
+      
     </>
   );
 }

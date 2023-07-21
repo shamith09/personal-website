66d06b0a384b101ca31cdbc0b2fc40d7f3dc0ebd
Shamith Pasula <shamith09@gmail.com>
2023-07-21 00:35:20 -0700

    favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import SPLogo from "../../public/Logo.svg";
 
 export default function Home() {
   return (
-    
-  )
+    <>
+      <Image src={SPLogo} alt="logo" />
+    </>
+  );
 }

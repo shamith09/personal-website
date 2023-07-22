2e1db7c1bc186e6b779966e27d1dc21bd076ebd0
Shamith Pasula <shamith09@gmail.com>
2023-07-21 17:20:40 -0700

    typewriter

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -1,11 +1,28 @@
+"use client";
+
 import styles from "./Name.module.css";
+import Typewriter from "typewriter-effect";
 
 export default function Name() {
   return (
     <div className={styles.container}>
       <h1 className={`${styles.title} title`}>SHAMITH PASULA</h1>
       <div className={styles.line} />
-      <h2 className={`${styles.subtitle} text`}>Software Engineer</h2>
+      <div className={`${styles.subtitle} text`}>
+        I am
+        <Typewriter
+          options={{
+            strings: [
+              " a Software Engineer.",
+              " a UC Berkeley student.",
+              " a full-stack developer.",
+              " an artist."
+            ],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </div>
     </div>
   );
 }

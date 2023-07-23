f80d06a200b7de4ead0d5f99e9ae0b88c629c0c7
Shamith Pasula <shamith09@gmail.com>
2023-07-23 01:39:11 -0700

    active link

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,23 +1,61 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./Links.module.css";
+import { usePathname } from "next/navigation";
 
 export default function Links() {
+  const pathname = usePathname();
+
   return (
     <div className={styles.container}>
-      <Link href="/about" className={`${styles.text} text`}>
-        About
+      <Link href="/">
+        <div
+          className={` ${styles.text} text ${
+            pathname === "/" && styles.active
+          }`}
+        >
+          Home
+        </div>
       </Link>
-      <Link href="/blog" className={`${styles.text} text`}>
-        Blog
+      <Link href="/about">
+        <div
+          className={`${styles.text} text ${
+            pathname === "/about" && styles.active
+          }`}
+        >
+          About
+        </div>
       </Link>
-      <Link href="/projects" className={`${styles.text} text`}>
-        Projects
+      <Link href="/blog">
+        <div
+          className={`${styles.text} text ${
+            pathname === "/blog" && styles.active
+          }`}
+        >
+          Blog
+        </div>
       </Link>
-      <Link href="/shamith-pasula-resume.pdf" className={`${styles.text} text`}>
-        Resume
+      <Link href="/projects">
+        <div
+          className={`${styles.text} text ${
+            pathname === "/projects" && styles.active
+          }`}
+        >
+          Projects
+        </div>
       </Link>
-      <Link href="/contact" className={`${styles.text} text`}>
-        Contact
+      <Link href="/shamith-pasula-resume.pdf">
+        <div className={`${styles.text} text`}>Resume</div>
+      </Link>
+      <Link href="/contact">
+        <div
+          className={`${styles.text} text ${
+            pathname === "/contact" && styles.active
+          }`}
+        >
+          Contact
+        </div>
       </Link>
     </div>
   );

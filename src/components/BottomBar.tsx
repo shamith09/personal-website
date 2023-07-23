d87e48b7a3637ef7263550016edab04d40f318bb
Shamith Pasula <shamith09@gmail.com>
2023-07-23 00:02:37 -0700

    fixed bottom style

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -3,11 +3,10 @@ import styles from "./BottomBar.module.css";
 
 export default function BottomBar() {
   return (
-    <>
-      <div className={styles.buffer} />
+    <div className={styles.buffer}>
       <div className={styles.container}>
         <p className={`${styles.text} text`}>Copyright © 2023</p>
       </div>
-    </>
+    </div>
   );
 }

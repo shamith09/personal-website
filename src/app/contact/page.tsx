dea4552eb7443a09c95d4c8f9b5ccdd292fcedb2
Shamith Pasula <shamith09@gmail.com>
2023-07-24 12:05:48 -0700

    interactive

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,36 +1,47 @@
 "use client";
 
-import { useState } from "react";
 import styles from "./page.module.css";
 import ContactIcons from "@/components/ContactIcons";
 import Link from "next/link";
 
-interface State {
-  name: string;
-  email: string;
-  subject: string;
-  message: string;
-}
-
 export default function MyComponent() {
-  const [state, setState] = useState<State>({
-    name: "",
-    email: "",
-    subject: "",
-    message: "",
-  });
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+
+    
+  };
 
   return (
     <>
       <ContactIcons />
       <div className={styles.center}></div>
       <div className={styles.right}>
-        <h1 className={`${styles.title} text`}>Got a question?</h1>
-        <div className={`${styles.form} text`}>Name</div>
-        <div className={`${styles.form} text`}>Email</div>
-        <div className={`${styles.form} text`}>Subject</div>
-        <div className={`${styles.form} ${styles.largeForm} text`}>Message</div>
-        <div className={`${styles.submit} text`}>Submit</div>
+        <form onSubmit={onSubmit}>
+          <div className={styles.formContainer}>
+            <h1 className={`${styles.title} text`}>Got a question?</h1>
+            <div className={`${styles.form} text`}>
+              <input name="name" placeholder="Name" className={styles.input} />
+            </div>
+            <div className={`${styles.form} text`}>
+              <input name="email" placeholder="Email" className={styles.input} />
+            </div>
+            <div className={`${styles.form} text`}>
+              <input name="subject" placeholder="Subject" className={styles.input} />
+            </div>
+            <div className={`${styles.form} ${styles.largeForm} text`}>
+              <textarea
+                name="message"
+                placeholder="Message"
+                className={styles.input}
+              />
+            </div>
+            <button type="submit" className={styles.button}>
+              <div className={`${styles.submit} text`}>Submit</div>
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );

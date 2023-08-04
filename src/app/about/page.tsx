8b66be131a099bec6b06feecf3a491a6a8603444
Shamith Pasula <shamith09@gmail.com>
2023-08-04 14:57:27 -0700

    sections

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -20,30 +20,38 @@ export default function About() {
 
   return (
     <div className={styles.container}>
-      <Subtitle>About me</Subtitle>
-      <div className={styles.paragraphContainer}>
-        <p className={`${styles.paragraph} text`}>
-          I&apos;m a third year at UC Berkeley studying Computer Science and
-          Astrophysics. I&apos;m passionate about building products that improve
-          our lives little by little, and I&apos;m not afraid of getting my
-          hands dirty and overengineering things.
-        </p>
-        <p className={`${styles.paragraph} text`}>
-          My interests lie in full-stack development, machine learning,
-          robotics, and systems engineering. I&apos;m always looking for
-          opportunities to learn new things and work with new people, so feel
-          free to visit the <Link href="/contact">Contact</Link> page and reach out!
-        </p>
+      <div className={styles.section}>
+        <Subtitle>About me</Subtitle>
+        <div className={styles.paragraphContainer}>
+          <p className={`${styles.paragraph} text`}>
+            I&apos;m a third year at UC Berkeley studying Computer Science and
+            Astrophysics. I&apos;m passionate about building products that
+            improve our lives little by little, and I&apos;m not afraid of
+            getting my hands dirty and overengineering things.
+          </p>
+          <p className={`${styles.paragraph} text`}>
+            My interests lie in full-stack development, machine learning,
+            robotics, and systems engineering. I&apos;m always looking for
+            opportunities to learn new things and work with new people, so feel
+            free to visit the <Link href="/contact">Contact</Link> page and
+            reach out!
+          </p>
+        </div>
       </div>
-      <div className={`${styles.statsAndInterestsContainer} text`}>
-        <div className={styles.statsContainer}>
-          <Subtitle>Stats</Subtitle>
-          <TableList items={stats} />
+      <div className={styles.section}>
+        <div className={`${styles.statsAndInterestsContainer} text`}>
+          <div className={styles.statsContainer}>
+            <Subtitle>Stats</Subtitle>
+            <TableList items={stats} />
+          </div>
+          <div className={styles.interestsContainer}>
+            <Subtitle>Interests</Subtitle>
+            <TableList items={interests} />
+          </div>
         </div>
-        <div className={styles.interestsContainer}>
-          <Subtitle>Interests</Subtitle>
-          <TableList items={interests} />
-        </div>
+      </div>
+      <div className={styles.section}>
+        <Subtitle>Experience</Subtitle>
       </div>
     </div>
   );

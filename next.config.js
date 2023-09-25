df7f84c5610876fe4c64d34694c79f9d50a51482
Shamith Pasula <shamith09@gmail.com>
2023-09-25 14:58:33 -0700

    resume redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/resume",
+        destination: "/Shamith_Pasula_resume.pdf",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
